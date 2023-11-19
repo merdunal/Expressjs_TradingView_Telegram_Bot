342e1238a90b444edd50bb247668dbb52dd178f5
merdunal <mertunal878@gmail.com>
2023-11-19 18:03:42 +0300

    update server url

--- index.js
+++ index.js
@@ -1,23 +1,23 @@
-require('dotenv').config()
-const express = require('express')
-const bodyParser = require('body-parser')
-const axios = require('axios')
+require('dotenv').config();
+const express = require('express');
+const bodyParser = require('body-parser');
+const axios = require('axios');
 
-const { TOKEN, SERVER_URL } = process.env
-const TELEGRAM_API = `https://api.telegram.org/bot${TOKEN}`
-const URI = `/webhook/${TOKEN}`
-const WEBHOOK_URL = SERVER_URL + URI
+const { TOKEN, SERVER_URL } = process.env;
+const TELEGRAM_API = `https://api.telegram.org/bot${TOKEN}`;
+const URI = `/webhook/${TOKEN}`;
+const WEBHOOK_URL = SERVER_URL + URI;
 
-const app = express()
-app.use(bodyParser.json())
+const app = express();
+app.use(bodyParser.json());
 
 const init = async () => {
-    const res = await axios.get(`${TELEGRAM_API}/setWebhook?url=${WEBHOOK_URL}`)
-    console.log(res.data)
-}
+    const res = await axios.get(`${TELEGRAM_API}/setWebhook?url=${WEBHOOK_URL}`);
+    console.log(res.data);
+};
 
 app.post(URI, async (req, res) => {
-    console.log(req.body)
+    console.log(req.body);
 
     if (req.body.tradingData) {
         const tradingData = req.body.tradingData;
@@ -37,10 +37,9 @@ app.post(URI, async (req, res) => {
     }
 
     return res.status(400).send('Bad Request: No trading data found in the request.');
-
 });
 
 app.listen(process.env.PORT || 4040, async () => {
-    console.log("app running on port", process.env.PORT || 4040)
-    await init()
-})
+    console.log("app running on port", process.env.PORT || 4040);
+    await init();
+});
